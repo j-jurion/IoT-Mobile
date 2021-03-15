52a56fa335cf882e0e746d9efebb5e9c303b627b
Joeri Jurion <51537901+j-jurion@users.noreply.github.com>
2021-03-15 20:10:48 +0100

    to main

--- hub/lib/constants/info.dart
+++ hub/lib/constants/info.dart
@@ -5,5 +5,5 @@ class Info {
   static const String description =
       'Hub is a platform to read and control your BLE (and soon IoT) devices.';
   static const String author = 'sandworm';
-  static const String version = 'v0.1.0';
+  static const String version = 'main';
 }
