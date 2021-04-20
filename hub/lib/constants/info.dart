53ab3c2b44e6a9137564356c7aa82ef4b29ada5d
Joeri Jurion <jurion@septentrio.com>
2021-04-20 19:06:53 +0200

    version 0.2.0

--- hub/lib/constants/info.dart
+++ hub/lib/constants/info.dart
@@ -5,5 +5,5 @@ class Info {
   static const String description =
       'Hub is a platform to read and control your BLE (and soon IoT) devices.';
   static const String author = 'sandworm';
-  static const String version = 'main';
+  static const String version = '0.2.0';
 }
